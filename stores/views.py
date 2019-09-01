73ad330753b5e2db7f4af5a0e5b17dbc1e90bf2e
Ahmad Qmairah <AhmadQmairah@gmail.com>
2019-09-01 10:42:44 +0300

    Ahmad & Shereen

--- stores/views.py
+++ stores/views.py
@@ -11,3 +11,22 @@ def store_list(request):
 
 
 
+def create_view(request):
+    form=StoreModelForm()
+
+    if request.method == "POST":
+        form=StoreModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+    context={"form":form}
+
+    return render(request, "create.html", context)
+
+def detail(request,store_slug):
+    form=StoreModelForm()
+    store=Store.objects.get(slug=store_slug)
+    context={"store":store}
+
+    return render(request, "detail.html", context)
+
